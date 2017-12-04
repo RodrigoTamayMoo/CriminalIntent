90bdd0d27303e6f00c289fe14fa1b1f3a09f73d4
RodrigoTamayMoo <14470439@itcampeche.edu.mx>
2017-12-04 10:10:42 -0600

    CriminalIntent: Responde a clics del usuario. Muestra un Toast

--- app/src/main/java/com/rodrigo_tamay_moo/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/rodrigo_tamay_moo/criminalintent/CrimeListFragment.java
@@ -12,6 +12,7 @@ import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -48,7 +49,8 @@ public class CrimeListFragment extends Fragment {
 
 
     // Punto Final
-    private class CrimeHolder extends RecyclerView.ViewHolder {
+    private class CrimeHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
         private Crime mCrime;
         private TextView mTitleTextView;
         private TextView mDateTextView;
@@ -56,6 +58,7 @@ public class CrimeListFragment extends Fragment {
 
         public CrimeHolder(View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
 
             mTitleTextView = (TextView) itemView
                     .findViewById(R.id.list_item_crime_title_text_view);
@@ -72,6 +75,13 @@ public class CrimeListFragment extends Fragment {
             mTitleTextView.setText(mCrime.getmTitle());
             mDateTextView.setText(mCrime.getDate().toString());
             mSolvedCheckBox.setChecked(mCrime.ismSolved());
+        }
+
+        @Override
+        public void onClick(View view){
+            Toast.makeText(getActivity(),
+                    "Se hizo click sobre " + mCrime.getmTitle(),
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
